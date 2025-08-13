170251658b80bf48f9d64e9d0c90d967d8599edb
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-14 03:12:28 +0600

    even average

---
+++ even_average.js
@@ -0,0 +1,19 @@
+const even = [];
+function evenAverage(numbers) {
+  for (const number of numbers) {
+    if (number % 2 === 0) {
+      even.push(number);
+    }
+  }
+  let sum = 0;
+  for (const number of even) {
+    sum = sum + number;
+  }
+  let count = even.length;
+  let avg = sum / count;
+  return avg;
+}
+
+const numbers = [42, 13, 58, 65, 81, 96, 7, 105];
+const avg = evenAverage(numbers);
+console.log(avg);
