c3f01b2efe11e7621146b5b1cb27c5b008a8c249
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-14 14:54:19 +0600

    math.

---
+++ math.js
@@ -0,0 +1,17 @@
+const min = Math.min(45, 21, 56, 99, 123, -99);
+console.log(min);
+const max = Math.max(45, 21, 56, 99, 123, -99);
+console.log(max);
+console.log(Math.PI);
+console.log(Math.abs(30 - 88));
+console.log(Math.round(4.8));
+console.log(Math.round(2.85));
+console.log(Math.floor(2.95));
+console.log(Math.floor(3.75));
+console.log(Math.ceil(2.00001));
+console.log(Math.ceil(3.0005));
+
+console.log(Math.random());
+console.log(Math.random() * 10);
+const rand = Math.round(Math.random() * 10);
+console.log(rand);
